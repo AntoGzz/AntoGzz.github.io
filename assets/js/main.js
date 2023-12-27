c36b8b4520575d0caa0eb35f94539287bc6087ea
AntoGz <quirozd70@gmail.com>
2023-12-27 15:07:47 -0400

    Add Analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 $(function(){
 
     $(window).on('load', function () {
-        $('.page-loader').delay('500').fadeOut(1000);
+        $('.page-loader').delay('100').fadeOut(300);
     });
 
     $(document).ready(function() {
